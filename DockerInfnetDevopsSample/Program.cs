1c2f901343aa34471b99686bc2d1a9be24ace8b4
ijuniobo <31112232+ijuniobo@users.noreply.github.com>
2023-09-09 15:13:17 -0300

    Refactor: ajustes novas cheks.

--- DockerInfnetDevopsSample/Program.cs
+++ DockerInfnetDevopsSample/Program.cs
@@ -1,3 +1,7 @@
+using DockerInfnetDevopsSample.HealthCheck;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHealthChecks()
+                .AddSqlServer(
+                    connectionString: builder.Configuration.GetConnectionString("InfnetPosDb"),
+                    healthQuery: "SELECT 1",
+                    name: "Database",
+                    failureStatus: Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus.Unhealthy
+                )
+                .AddUrlGroup(new Uri("http://httpbin.org/status/200"), "Api Terceiro Nao Autenticada")
+                .AddCheck<HealthCheckRandom>(name: "Api Terceiro Autenticada");
+
+
+builder.Services.AddHealthChecksUI(s =>
+{
+    s.AddHealthCheckEndpoint("Infnet API", "https://localhost:44365/healthz");
+})
+.AddInMemoryStorage();
+
+
 
 var app = builder.Build();
 
@@ -21,6 +44,19 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseRouting()
+   .UseEndpoints(config =>
+   {
+       config.MapHealthChecks("/healthz", new HealthCheckOptions
+       {
+           Predicate = _ => true,
+           ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+       });
+
+       config.MapHealthChecksUI();
+   });
+
 
 app.Run();
 
